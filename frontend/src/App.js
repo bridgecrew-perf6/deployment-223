ee5076e78acaee03469ac490598bb182751b6705
Zulfiqar-Ali <52883632+zulfiqarAlibalti@users.noreply.github.com>
2022-02-28 01:05:46 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
   handlePredictClick = (event) => {
     const formData = this.state.formData;
     this.setState({ isLoading: true });
-    fetch('http://127.0.0.1:8000/prediction/', 
+    fetch('https://ml-drug-prediction-model.herokuapp.com/', 
       {
         headers: {
           'Accept': 'application/json',
